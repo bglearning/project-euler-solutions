d5b4b39f02d08545ebb96ad8c7a4d0eb43c05c3f
Bijay Gurung <bjgurung10@gmail.com>
2016-10-04 21:44:05 +0545

    Add Method 2 to Problem 25
    The original method (Method 1) used a brute force approach.
    
    Instead, we can make use of the direct formulae for the Fibonacci series.

--- solutions/prob25.py
+++ solutions/prob25.py
@@ -1,10 +1,15 @@
 # Problem 25
 # Index of first Fibonacci number to have 1000 digits
 
+from math import sqrt
+from math import log
+
+limit = 1000
+
+# Method 1
 lower = 1
 higher = 1
 index = 2
-limit = 1000
 
 while True:
     if len(str(higher)) >= limit:
@@ -13,3 +18,19 @@ while True:
     index += 1
 
 print(index)
+
+# Method 2
+
+# The nth Fibonacci number is [phi**n / sqrt(5)], where the
+# brackets denote "nearest integer".
+#
+# So we need phi**n/sqrt(5) > 10**999
+#
+# n * log(phi) - log(5)/2 > 999 * log(10)
+#
+# n * log(phi) > 999 * log(10) + log(5)/2
+# n > (999 * log(10) + log(5) / 2) / log(phi)
+
+phi = (1 + sqrt(5)) / 2
+n = int(((limit - 1) * log(10) + log(5) / 2) / log(phi))
+print(n + 1)
